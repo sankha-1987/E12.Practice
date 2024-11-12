eb8adc447f2c5ec2763bdf69d8200a5eb4ea9cb4
Sankhadeep <sankha.cse@gmail.com>
2024-11-12 20:43:51 +0530

    sample2 package created. Demo 200 and Demo201 class is created.

---
+++ src/test/java/sample2/Demo200.java
@@ -0,0 +1,11 @@
+package sample2;
+
+public class Demo200 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Demo200 package - first file created");
+
+	}
+
+}
---
+++ src/test/java/sample2/Demo201.java
@@ -0,0 +1,9 @@
+package sample2;
+
+public class Demo201 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Demo201 - Second file created");
+	}
+}
