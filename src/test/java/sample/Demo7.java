111d9194513db09e48aa9af5eae2402ba76d764c
Sankhadeep <sankha.cse@gmail.com>
2024-11-12 20:37:31 +0530

    This is seventh Commit

---
+++ src/test/java/sample/Demo7.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Demo7 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is created after Demo, Demo2, Demo3, Demo4, Demo5, Demo6");
+
+	}
+
+}
